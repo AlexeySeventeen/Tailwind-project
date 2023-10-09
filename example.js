34376c521c6b6757f4a268775a84c0722ec752c7
AlexeySeventeen <AlexeyVileiskij@yandex.ru>
2023-10-09 20:52:01 +0300

    tasks from codewars

--- example.js
+++ example.js
@@ -1,22 +1,13 @@
-function dblLinear(n) {
-  // todo: make code faster (too much numbers in algorithm)
-  // ?how uniq work
-  // ?how sort work
-
-  const res = [1];
-
-  for (let index = 0; index < n; index++) {
-    const new1 = res[index] * 2 + 1;
-    res.push(new1);
-    const new2 = res[index] * 3 + 1;
-    res.push(new2);
-    res.sort((a, b) => a - b);
-  }
-
-  const uniqObj = new Set(res);
-  const uniqArray = Array.from(uniqObj);
-
-  return uniqArray;
+function determinant(matrix) {
+  const array = [];
+  array.length = (matrix.length - 1) ;
+  console.log(array.fill([], 0));
 }
 
-console.log(dblLinear(10));
+console.log(
+  determinant([
+    [2, 4, 2],
+    [3, 1, 1],
+    [1, 2, 0],
+  ])
+);
