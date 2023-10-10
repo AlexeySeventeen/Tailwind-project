287df9cd555c11dff97e49718d1e7e9f862a6200
AlexeySeventeen <AlexeyVileiskij@yandex.ru>
2023-10-10 22:14:38 +0300

    ddd

--- example.js
+++ example.js
@@ -1,9 +1,23 @@
 function determinant(matrix) {
   const array = [];
-  array.length = (matrix.length - 1) ;
+  array.length = matrix.length - 1;
   console.log(array.fill([], 0));
 }
 
+console.log(
+  determinant([
+    [2, 4, 2],
+    [3, 1, 1],
+    [1, 2, 0],
+  ])
+);
+console.log(
+  determinant([
+    [2, 4, 2],
+    [3, 1, 1],
+    [1, 2, 0],
+  ])
+);
 console.log(
   determinant([
     [2, 4, 2],
