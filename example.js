06a924410d8d1cab1c06da924cf0115a3a7c17f2
AlexeySeventeen <AlexeyVileiskij@yandex.ru>
2023-10-10 22:16:12 +0300

    we return

--- example.js
+++ example.js
@@ -11,17 +11,3 @@ console.log(
     [1, 2, 0],
   ])
 );
-console.log(
-  determinant([
-    [2, 4, 2],
-    [3, 1, 1],
-    [1, 2, 0],
-  ])
-);
-console.log(
-  determinant([
-    [2, 4, 2],
-    [3, 1, 1],
-    [1, 2, 0],
-  ])
-);
