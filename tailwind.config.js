449b7c646da6e1bfcdc1e8584b2d024e3a2c3feb
AlexeySeventeen <AlexeyVileiskij@yandex.ru>
2023-10-12 15:15:42 +0300

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
-      //? https://tailwindcss.com/docs/theme - смотреть какой аспект изменить
       gridTemplateRows: {
         layout: 'auto 1fr auto',
       },
